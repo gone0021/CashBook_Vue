ebab34f252cb036e59fdf4d8e2f9e261a3a54382
gone0021 <gt.4.pff.gi@gmail.com>
2021-04-15 20:37:16 +0900

    追加：バクエンドからのバリデーションエラーのメッセージ表示のモーダル

---
+++ src/resources/js/components/modalMsg.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="modalMsg" class="">
+    <p>内容に不備があり登録に失敗しました</p>
+    <p>再度入力をお願いします</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // ただのエラーメッセージ
+};
+</script>
